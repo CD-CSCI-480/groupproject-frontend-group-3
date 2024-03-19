5aa8568b115c79631c4342c5fed5210e31ecd5f2
DaFDE31 <adjocydanielson@gmail.com>
2024-03-18 21:05:48 -0400

    Added styling and started doing diseases

---
+++ src/app/[disease].tsx
@@ -0,0 +1,37 @@
+import {Stack, useLocalSearchParams } from 'expo-router';
+import {View, Text, Image, StyleSheet, Pressable} from 'react-native'
+import { useState } from 'react';
+
+const DiseaseScreen = () =>{
+    <View style = {style.container}>
+        <Stack.Screen options={{title: "Diseases"}}/>{// should be changed to the specific disease
+
+        }
+    </View>
+
+}
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        justifyContent: "center",
+        padding: 10,
+    },
+    image: {
+        width: "80%",
+        aspectRatio: 1,
+        margin: 10,
+        backgroundColor: "BBE14E",
+        borderRadius: 20,
+    },
+    textContainer: {
+
+    },
+    text: {
+        fontFamily: "Georgia", 
+        fontSize: 12,
+        fontWeight: "500",
+        color: "black",// will be add a conditional to make this white on dark mode.
+    },
+})
+
+export default DiseaseScreen;
