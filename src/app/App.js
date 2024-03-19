34fd31efde49995150701a583d565c5ed3588b05
DaFDE31 <adjocydanielson@gmail.com>
2024-03-18 20:01:53 -0400

    Added pics

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+//import {TabLayout} from "src/app/tabstemp";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style= {styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    color: "blue",
+    fontWeight: "bold",
   },
 });
