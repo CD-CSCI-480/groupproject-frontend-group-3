66d315ed3b0715fedfb829dde9e469023aef6dc0
ceasar <mrpereirac@gmail.com>
2024-03-20 18:21:30 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Antibiotics from './src/app/Antibiotics';
 
 export default function App() {
   return (
-  //  <View style={styles.container}>
-   //   <Text>Open up App.js to start working on your app!</Text>
-      <Antibiotics />
-//<StatusBar style="auto" />
-  //  </View>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app! Please </Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
