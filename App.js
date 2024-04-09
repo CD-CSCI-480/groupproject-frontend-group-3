3f9aa8779d70fd912d2016eb2732862b1f72b6c4
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-04-08 23:15:53 -0400

    added some navigation using react-navigation

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Reminders } from './src/app/screens/Reminders';
+import { HomeScreen } from './src/app/screens/Home/HomeScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <Reminders />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Reminders' component={Reminders} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
