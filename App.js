39c9747279590e0e8ebcfef85b1567e78d7f863c
Danielson Adjocy <dadjocy21@Danielsons-MacBook-Pro.local>
2024-04-15 10:57:27 -0400

    hello

--- App.js
+++ App.js
@@ -4,14 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import MenuScreen from './src/app/(tabs)/menu/menu';
 import DiseaseScreen from './src/app/(tabs)/[disease]';
 export default function App() {
-  
-  
-  //return (
-  //  <View style={styles.container}>
-  //    <Text style= {styles.text}>Open up App.js to start working on your app!</Text>
-  //    <StatusBar style="auto" />
-  //  </View>
-  //);
+
   return(
     <DiseaseScreen/>
   );
