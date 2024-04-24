8bcdd49ce3c4227105f19c5f80d001ea843c3f86
ceasar <mrpereirac@gmail.com>
2024-04-23 20:47:55 -0400

    homescreen and styles file

--- src/app/screens/Home/styles.js
+++ src/app/screens/Home/styles.js
@@ -1,16 +1,27 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F2F2F2',
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    text:{
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 16,
-        color:"#003006"
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#003006',
+  },
+  chatBubble: {
+    backgroundColor: '#0B93F6',
+    borderRadius: 50,
+    padding: 16,
+    position: 'relative',
+    alignSelf: 'flex-end',
+    marginRight: 16,
+  },
+  calendarIcon: {
+    fontSize: 30,
+    color: 'white',
+  },
 });
